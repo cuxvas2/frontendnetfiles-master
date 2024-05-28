f82953010ebb48414190c39f7033c97ef342db53
Victor <cuevas0122@gmail.com>
2024-05-28 04:57:28 -0600

    first commit

---
+++ Services/PerfilClientService.cs
@@ -0,0 +1,9 @@
+﻿namespace frontendnet.Services;
+
+public class PerfilClientService(HttpClient client)
+{
+    public async Task<string> ObtenTiempoAsync()
+    {
+        return await client.GetStringAsync($"api/auth/tiempo");
+    }
+}
